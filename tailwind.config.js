56176d050736c67b73ae30512eb7c8b19327276b
Dean Kim <deankim1118@gmail.com>
2024-05-14 16:18:59 -0400

    1. UI - Set Global CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    colors: {
+      'dark-blue': '#0a1930',
+      'light-blue': '#1f93ff',
+
+      'light-gray': '#E7E7E7',
+      'dark-gray': '#767676',
+      'border-gray': '#CCCCCC',
+      'table-border-gray': '#EEE',
+
+      'color-white': ' #FFFFFF',
+      'color-primary': '#0074e9',
+
+      'color-black': '#111111',
+      'color-red': '#E52528',
     },
   },
   plugins: [],
